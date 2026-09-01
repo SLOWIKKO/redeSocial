6d45b420c6e339311adf8ffef5c45e4c37ad80b9
slowik da silva <gbslowik@gmail.com>
2026-09-01 09:02:40 -0300

    fafasdfasdf

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
     const likeBtn = document.querySelector (".like-btn");
     const postMedia = document.querySelector (".postMedia");
+if (!likeBtn) return;
+
+   const likesCountSpan = LikeBtn.querySelector (".like-btn");
+   const bookmarkBtn = document.querySelector
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
